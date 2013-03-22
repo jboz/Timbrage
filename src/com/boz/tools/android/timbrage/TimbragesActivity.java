9eaff2cbcb3abac7b575ed1a0253f3a99e55031a
jboz <julienboz@gmail.com>
2013-03-22 15:13:36 +0100

    add in one text field

--- src/com/boz/tools/android/timbrage/TimbragesActivity.java
+++ src/com/boz/tools/android/timbrage/TimbragesActivity.java
@@ -1,20 +1,43 @@
 package com.boz.tools.android.timbrage;
 
-import android.os.Bundle;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TimbragesActivity extends Activity {
+    
+    private final DateFormat FORMATTER = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timbrages);
+
+        final Button addBtn = (Button) findViewById(R.id.btnAdd);
+        addBtn.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                addTime();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_timbrages, menu);
         return true;
+    }
+
+    public void addTime() {
+        ((TextView) findViewById(R.id.helloTxt)).setText(FORMATTER.format(new Date()));
+        System.out.println(new Date());
     }
 }
