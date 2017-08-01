f74fdbbf585b92ce37e27eed8c9165c60a662669
jboz <julienboz@gmail.com>
2017-08-01 16:50:37 +0200

    beta-3

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,5 +1,24 @@
-cd ..
+#!/bin/bash
+
 ionic cordova plugin rm cordova-plugin-console
+
+echo '
+
+BUILDING android app..
+
+'
 ionic cordova build android --release --prod
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+
+echo '
+
+SIGNING android app..
+
+'
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .travis/release.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+
+echo '
+
+FINISHING android app..
+
+'
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk timbrage.apk
