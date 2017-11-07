0fc40df1fe7d8088788162f2f5da1cada2fc073d
jboz <julienboz@gmail.com>
2017-11-07 20:17:59 +0100

    new keystore

--- .travis/release.local.sh
+++ .travis/release.local.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+output_apk=timbrage.apk
+
+if [ -e $output_apk ]
+then
+    echo "Output file '$output_apk' exists !"
+    exit 0;
+fi
+
 read -s -p "Enter Passphrase for keystore: " storepass
 
 #ionic cordova plugin rm cordova-plugin-console
@@ -16,11 +24,11 @@ echo '
 SIGNING android app..
 
 '
-jarsigner -storepass $storepass -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .travis/release.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+jarsigner -storepass $storepass -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore .travis/release-keystore.jks platforms/android/build/outputs/apk/android-release-unsigned.apk timbrage-app
 
 echo '
 
 FINISHING android app..
 
 '
-~/Documents/prog/tools/android-sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk timbrage.apk
+~/Documents/prog/tools/android-sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk $output_apk
