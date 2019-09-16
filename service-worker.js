ead32c6db02240a9c431b0e32e7a1ca5292fdf2d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-16 07:21:06 +0000

    Deploy jboz/timbrage to github.com/jboz/timbrage.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Copyright (C) 2017 Julien Boz
+// 
+// This file is part of Focus IT - Timbrage.
+// 
+// Focus IT - Timbrage is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Focus IT - Timbrage is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Focus IT - Timbrage.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+/**
+ * Check out https://googlechrome.github.io/sw-toolbox/ for
+ * more info on how to use sw-toolbox to custom configure your service worker.
+ */
+
+
+'use strict';
+importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    './build/main.js',
+    './build/vendor.js',
+    './build/main.css',
+    './build/polyfills.js',
+    'index.html',
+    'manifest.json'
+  ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;
